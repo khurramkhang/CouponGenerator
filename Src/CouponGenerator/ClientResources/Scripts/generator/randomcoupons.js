9fd55044a1e7701ed8549730742ae8cd647d3ea0
khurram khan <khurramkhang@outlook.com>
2016-04-28 19:52:11 +0100

    Project version 1.0.0 is added

---
+++ Src/CouponGenerator/ClientResources/Scripts/generator/randomcoupons.js
@@ -0,0 +1,67 @@
+﻿define([
+
+// Dojo
+    "dojo/dom-construct",
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/html",
+    "dojo/on",
+
+// Dijit
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetBase",
+
+//CMS 
+    "epi-cms/_ContentContextMixin",
+
+//Resources
+    "dojo/text!coupon/generator/templates/randominputs.html"
+], function (
+
+// Doj
+    domConstruct,
+    declare,
+    lang,
+    html,
+    on,
+
+// Dijit
+    _TemplatedMixin,
+    _WidgetBase,
+
+//CMS
+    _ContentContextMixin,
+
+//Resources
+    template
+) {
+    return declare([_WidgetBase, _TemplatedMixin, _ContentContextMixin], {
+        templateString: template,
+        //filter: null,
+        //dijitForms: null,
+        //checkedItems: null,
+
+        constructor: function () {
+            //this.dijitForms = [];
+            //this.checkedItems = [];
+        },
+        _onButtonClick: function () {
+            var chars = this.anChars.value;
+            var tokenLength = this.anLength.value;
+            var totalToken = this.anNoOfCoupons.value;           
+            var randomstring = '';
+            for (var j = 0; j < totalToken; j++) {
+                randomstring += "<div  style='padding: 5px;'>" + this.anPrefix.value;
+                for (var i = 0; i < tokenLength; i++) {
+                    var rnum = Math.floor(Math.random() * chars.length);
+                    randomstring += chars.substring(rnum, rnum + 1);
+                }
+                randomstring += this.anPostfix.value;
+                randomstring += "</div>";
+            }
+            html.set(this.coupon_random_results, randomstring);
+            //this.coupon_random_results.text(chars);
+        }
+
+    });
+});
---
+++ Src/CouponGenerator/Coupon.Generator/RandomCoupons.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Shell.ViewComposition;
+
+namespace EpiServerCommerce.Coupon.Generator
+{
+    [Component]
+    public class RandomCoupons : ComponentDefinitionBase
+    {
+        public RandomCoupons()
+            : base("coupon.generator.randomcoupons")
+        {
+            Title = "Random Coupons";
+            Description = "Generate multiple random Alpha Numeric coupon codes";
+            SortOrder = 500;
+            PlugInAreas = new[] { "/episerver/cms/assets/defaultgroup", "/episerver/commerce/assets/defaultgroup" };
+            Categories = new[] { "commerce" };
+        }
+    }
+
+}
